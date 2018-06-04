9d9ca3df557b70e7b696dccdf711e1260d8ef11a
Lucian Toma <tomalucian11@gmail.com>
2018-06-04 14:56:14 +0300

    added image-card component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SmartHouse!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to PWCat!');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { 
+  MatToolbarModule,   //Toolbar
+  MatCardModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { ImgCardComponent } from './img-card/img-card.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,  //Material Toolbar
+    MatCardModule      //Material Card Module
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
