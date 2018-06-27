740239a4a5335523b01d82e01f07816c9ed3b456
Lucian Toma <tomalucian11@gmail.com>
2018-06-28 00:01:57 +0300

    Fixed a build-crash

--- src/app/device-card/appliance/appliance.component.ts
+++ src/app/device-card/appliance/appliance.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './appliance.component.html',
   styleUrls: ['./appliance.component.css']
 })
+
 export class ApplianceComponent implements OnInit {
 
   constructor() { }
